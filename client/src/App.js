e2e88aab1092c6f70cad11de1afa72f62d3525f1
YoSoyPan <45904881+yosoypan@users.noreply.github.com>
2021-09-14 18:56:41 -0500

    Finished setting up repo in prep for site layout, multiple changes in React and assets uploaded in Public.

---
+++ client/src/App.js
@@ -0,0 +1,11 @@
+import Landing from "./components/Landing.js"
+
+function App() {
+  return (
+    <div className="App">
+      <Landing/>
+    </div>
+  );
+}
+
+export default App;
---
+++ client/src/components/Landing.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function landing() {
+    return (
+        <div className="root-wrapper">
+            <p>test</p>
+        </div>
+    )
+}
+
+export default landing
