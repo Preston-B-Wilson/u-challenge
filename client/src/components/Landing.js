416678344ebf45f25caef65395f30890cda066b6
YoSoyPan <45904881+yosoypan@users.noreply.github.com>
2021-09-14 19:25:37 -0500

    1st hour upload, finished site html blocks and image/svg links

--- client/src/components/Landing.js
+++ client/src/components/Landing.js
@@ -3,7 +3,21 @@ import React from 'react'
 function landing() {
     return (
         <div className="root-wrapper">
-            <p>test</p>
+            <div className="main-container" style={{ backgroundImage: 'url("./images/Coding-Challenge-Image.png")' }}>
+                <div className="sidebar-container">
+                    <div className="sidebar-content">
+                        <h1 className="sidebar-title">Explore</h1>
+                        <div className="sidebar-description-content">
+                            <input id="plus-logo" type="image" src={process.env.PUBLIC_URL + "/images/Plus.svg"}/>
+                            <p id="sidebar-description">More Details</p>
+                        </div>
+                    </div>
+                </div>
+                <div className="social-icons-container">
+                    <input id="facebook-logo" type="image" src={process.env.PUBLIC_URL + "/images/Facebook.svg"}/>
+                    <input id="instagram-logo" type="image" src={process.env.PUBLIC_URL + "/images/Instagram.svg"}/>
+                </div>
+            </div>
         </div>
     )
 }
