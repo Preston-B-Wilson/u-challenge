07fc525a7099d3dad75df6f60631f70a7f038985
YoSoyPan <45904881+yosoypan@users.noreply.github.com>
2021-09-17 20:34:12 -0500

    Responsiveness complete

--- client/src/components/Landing.js
+++ client/src/components/Landing.js
@@ -5,11 +5,13 @@ function landing() {
         <div className="root-wrapper">
             <div className="main-container" style={{ backgroundImage: 'url("./images/Coding-Challenge-Image.png")' }}>
                 <div className="sidebar-container">
-                    <div className="sidebar-content">
-                        <h1 className="sidebar-title">Explore</h1>
-                        <div className="sidebar-description-content">
-                            <input id="plus-logo" type="image" src={process.env.PUBLIC_URL + "/images/Plus.svg"}/>
-                            <p id="sidebar-description">More Details</p>
+                    <div className="sidebar-spacer">
+                        <div className="sidebar-content">
+                            <h1 className="sidebar-title">Explore</h1>
+                            <div className="sidebar-description-content">
+                                <input id="plus-logo" type="image" src={process.env.PUBLIC_URL + "/images/Plus.svg"}/>
+                                <p id="sidebar-description">More Details</p>
+                            </div>
                         </div>
                     </div>
                 </div>
